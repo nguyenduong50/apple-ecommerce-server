b6d5ccd5f452f587440b0b6deab1be97317fce87
nguyenduong <duonghp1991@gmail.com>
2024-05-11 12:56:37 +0700

    update env

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: 'dacjg1yin',
+  api_key: '288511963769949',
+  api_secret: 'omKJ7cN0xaobuRA9IYg3F0AIHVQ'
+})
+
+export default cloudinary
